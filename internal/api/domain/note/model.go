f89df14e1c083b7f83d2493f12b4938921978bff
Tommy Lay <tommylay.c@gmail.com>
2026-02-10 15:30:05 -0800

    refactor: remove unecessary files/folders from code base, got rid of any lingering schema definitions in model.go files

--- internal/api/domain/note/model.go
+++ internal/api/domain/note/model.go
@@ -17,17 +17,3 @@ type NoteKeywords struct {
 	ID     *uuid.UUID
 	NoteID uuid.UUID
 }
-
-const NoteSchema = `
-	CREATE TABLE note(
-	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	creation_date TEXT NOT NULL,
-	modification_date TEXT NOT NULL,
-	content TEXT
-	)`
-
-const NoteKeywordsSchema = `
-	CREATE TABLE note_keyword(
-	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	note_id UUID NOT NULL
-	)`
